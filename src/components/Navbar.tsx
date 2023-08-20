4fb53b482a1e19e38a91ef672b27e94fbba2b5e3
agasefamircan <agasefemircanov@gmail.com>
2023-08-20 23:56:21 +0400

    navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/images/logopng.svg";
 import "semantic-ui-css/semantic.min.css";
-import styles from '../assets/Navbar/navbar.module.css'
+import styles from "../assets/Navbar/navbar.module.css";
 
 const Navbar = () => {
   return (
@@ -9,19 +9,30 @@ const Navbar = () => {
       <div className={styles.logo}>
         <img className={styles.img} src={Logo} alt="logo" />
       </div>
+      <div className={styles.navItems}>
+        <Link className={styles.item} to="/">
+          Home
+        </Link>
+        <Link className={styles.item} to="/about">
+          About
+        </Link>
+        <Link className={styles.item} to="/contact">
+          Contact
+        </Link>
+      </div>
       <div className={styles.details}>
         <div className="cart">
-          <i className="shopping basket icon"></i>
+          <i className="shopping large olive basket icon"></i>
         </div>
         <div className={styles.account}>
           <div className="login">
             <Link to="/login">
-              <button className="ui primary button">Login</button>
+              <button className="ui green button">Login</button>
             </Link>
           </div>
           <div className="signup">
             <Link to="/signup">
-              <button className="ui button">Sign Up</button>{" "}
+              <button className="ui teal button">Sign Up</button>{" "}
             </Link>
           </div>
         </div>
