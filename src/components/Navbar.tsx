f6d7cc741af05abaa6d3fe9ee68726fb892e86cf
agasefamircan <agasefemircanov@gmail.com>
2023-08-20 23:39:33 +0400

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/logopng.png";
+import Logo from "../assets/images/logopng.svg";
 import "semantic-ui-css/semantic.min.css";
+import styles from '../assets/Navbar/navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="logo">
-        <img src={Logo} alt="logo" />
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <img className={styles.img} src={Logo} alt="logo" />
       </div>
-      <div className="details">
-        <div className="cart"></div>
-        <div className="account">
+      <div className={styles.details}>
+        <div className="cart">
+          <i className="shopping basket icon"></i>
+        </div>
+        <div className={styles.account}>
           <div className="login">
             <Link to="/login">
               <button className="ui primary button">Login</button>
