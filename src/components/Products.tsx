307c2666208af1953c9d37bad64e0e716ebb33f9
agasefamircan <agasefemircanov@gmail.com>
2023-08-23 23:47:27 +0400

    product type added

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 import axios from "axios";
 import style from "../assets/css/products.module.css";
 import { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 const Products = () => {
   const [loading, setLoading] = useState(0);
@@ -19,7 +20,8 @@ const Products = () => {
       try {
         const res = await axios.get("https://fakestoreapi.com/products");
         setData(res.data);
-        setFiltered(res.data)
+        setFiltered(res.data);
+        setLoading(0);
       } catch (error) {
         console.log("ERROR :", error);
       }
@@ -27,74 +29,76 @@ const Products = () => {
     };
     fetchProduct();
   }, []);
-   
-  const cliced=()=>{
-    alert('clicked')
-  }
 
   return (
-    <div className={style.products}>
-      <div className={style.filterButtons}>
-        <button
-          className="ui inverted blue button"
-          onClick={() => setFiltered(data)}
-        >
-          All
-        </button>
-        <button
-          className="ui inverted blue button"
-          onClick={() => filterProduct("electronics")}
-        >
-          Electronics
-        </button>
-        <button
-          className="ui inverted blue button"
-          onClick={() => filterProduct("jewelery")}
-        >
-          Accessories
-        </button>
-        <button
-          className="ui inverted blue button"
-          onClick={() => filterProduct("men's clothing")}
-        >
-          Men's clothes{" "}
-        </button>
-        <button
-          className="ui inverted blue button"
-          onClick={() => filterProduct("women's clothing")}
-        >
-          Women's clothes
-        </button>
-      </div>
-      <div className={style.productList}>
-        {filtered.map((product, i) => (
-          <div className={style.product} key={i}>
-            <div className={style.card}>
-              <img
-                className={style.img}
-                src={product.image}
-                alt={product.title}
-              />
-              <NavLink
-                to={`/product/${product.id}`}
-                className={style.cardTitle}
-              >
-                {product.title.substring(0, 20)}...
-              </NavLink>
-              <div className={style.productDetails}>
-                <div className={style.price}>
-                  <b>{product.price} $</b>
-                </div>
-                <div className={style.buttons}>
-                  <i className="shopping large teal basket icon"></i>
-                  <i className="heart large teal basket icon"></i>
+    <>
+      <div className={style.products}>
+        <div className={style.filterButtons}>
+          <button
+            className="ui inverted blue button"
+            onClick={() => setFiltered(data)}
+          >
+            All
+          </button>
+          <button
+            className="ui inverted blue button"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronics
+          </button>
+          <button
+            className="ui inverted blue button"
+            onClick={() => filterProduct("jewelery")}
+          >
+            Accessories
+          </button>
+          <button
+            className="ui inverted blue button"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's clothes{" "}
+          </button>
+          <button
+            className="ui inverted blue button"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's clothes
+          </button>
+        </div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className={style.productList}>
+            {filtered.map((product, i) => (
+              <div className={style.product} key={i}>
+                <div className={style.card}>
+                  <img
+                    className={style.img}
+                    src={product.image}
+                    alt={product.title}
+                  />
+                  <NavLink
+                    to={`/product/${product.id}`}
+                    className={style.cardTitle}
+                  >
+                    {product.title.substring(0, 20)}...
+                  </NavLink>
+                  <div className={style.productDetails}>
+                    <div className={style.price}>
+                      <b>{product.price} $</b>
+                    </div>
+                    <div className={style.buttons}>
+                      <i className="shopping large teal basket icon"></i>
+                      <i className="heart large teal basket icon"></i>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/store/reducers/productSlice.ts
@@ -0,0 +1,50 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Product {
+  id: number;
+  title: string;
+  category: string;
+}
+
+interface ProductsState {
+  products: Product[];
+  filteredProducts: Product[];
+  loading: boolean;
+}
+
+const initialState: ProductsState = {
+  products: [],
+  filteredProducts: [],
+  loading: false,
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    setProducts(state, action: PayloadAction<Product[]>) {
+      state.products = action.payload;
+      console.log("setProduct action payload:", action);
+    },
+    setFilteredProducts(state, action: PayloadAction<Product[]>) {
+      state.filteredProducts = action.payload;
+    },
+    setLoading(state, action: PayloadAction<boolean>) {
+      state.loading = action.payload;
+    },
+  },
+});
+export const { setProducts, setFilteredProducts } = productsSlice.actions;
+export default productsSlice.reducer;
+
+export const fetchProducts = (): AppThunk => async (dispatch) => {
+    dispatch(setLoading(true));
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      dispatch(setProducts(response.data));
+      dispatch(setLoading(false));
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      dispatch(setLoading(false));
+    }
+  };
